7620af2b5715ed4b085f79c329a4d90be0d9038e
Yousuf <aarajyousuf.qtec@gmail.com>
2021-03-08 12:07:38 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:dynamic_tabbar_flutter/custom_tab.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> data = ['Page 0', 'Page 1', 'Page 2', 'Page 3'];
+  int initPosition = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: CustomTabView(
+          initPosition: initPosition,
+          itemCount: data.length,
+          tabBuilder: (context, index) => Tab(text: data[index]),
+          pageBuilder: (context, index) => Center(child: Text(data[index])),
+          onPositionChange: (index) {
+            print('current position: $index');
+            initPosition = index;
+          },
+          onScroll: (position) => print('$position'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            data.add('Page ${data.length}');
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+
